bfd5ef5edb23b18c8888598a264ac4687ed58d5a
luc4sleite <lucahleite@gmail.com>
2023-08-25 17:50:29 -0300

    excluir e editar serviço na área administrativa

--- mysql/SCRIPT_INICIAL.sql
+++ mysql/SCRIPT_INICIAL.sql
@@ -1,1 +1,39 @@
 create database goservice_db;
+
+use goservice_db;
+
+show tables;
+
+insert into usuarios (dtype, nome, email, perfil, senha, habilitado)
+value ( 
+	"Administrador",
+	"Lucas Leite",
+    "lucas@mail.com",
+    "ADMIN",
+    "$2a$10$r3r9V682sIhE/61jZjqkauT.08pxrBx.GE1T.yEogN5r8Ly2S8eTK",
+    1
+); # 123456
+
+select * from usuarios;
+
+select * from agendamentos;
+
+select * from prestadores_servicos;
+
+
+UPDATE usuarios SET habilitado = 1 where id in (2, 3, 4);
+
+select * from servicos;
+
+INSERT INTO servicos (categoria, descricao, nome) 
+VALUES 
+    ('Limpeza', 'Serviço de limpeza residencial', 'Limpeza Residencial'),
+    ('Treinamento', 'Treinamento corporativo em liderança', 'Treinamento em Liderança'),
+    ('Saúde', 'Consulta médica de clínico geral', 'Consulta Clínico Geral'),
+    ('Desenvolvimento', 'Desenvolvimento de aplicativo móvel', 'Desenvolvimento de App'),
+    ('Design', 'Design de logotipo para empresas', 'Design de Logotipo'),
+    ('Limpeza', 'Limpeza de escritórios comerciais', 'Limpeza Comercial'),
+    ('Treinamento', 'Treinamento em habilidades de comunicação', 'Treinamento em Comunicação'),
+    ('Saúde', 'Sessão de fisioterapia para reabilitação', 'Fisioterapia de Reabilitação'),
+    ('Desenvolvimento', 'Desenvolvimento de site institucional', 'Desenvolvimento de Site Institucional'),
+    ('Design', 'Design de interface de usuário para aplicativos', 'Design de Interface de Aplicativo');
